4eaf28f5bf985f45bc2320bb0020c3f52dda9309
4497563 <24048040@myrp.edu.sg>
2025-07-23 01:59:24 +0000

    delete-function commit

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-test 123
+app.get('/deleteHabit/:id', (req, res) => {
+    const habitId = req.params.id;
+
+    connection.query('DELETE FROM products WHERE habitId = ?', [habitId], (error, results) => {
+        if (error) {
+            console.error("Error deleting habit:", error);
+            res.status(500).send('Error deleting habit');
+        } else {
+            // Send a success response
+            res.redirect('/habitlist');
+        }
+    });
+});

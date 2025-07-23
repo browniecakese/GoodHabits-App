3ddf06934ae9f2a696b070acfb0e10f9615d887c
Kaera Barber <23031962@myrp.edu.sg>
2025-07-23 10:01:57 +0800

    app base commit

--- app.js
+++ app.js
@@ -1,3 +1,78 @@
+const express = require('express');
+const mysql = require('mysql2');
+const session = require('express-session');
+const flash = require('connect-flash');
+const multer = require('multer');
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname); 
+    }
+});
+
+const upload = multer({ storage: storage });
+
+const connection = mysql.createConnection({
+    host: 'c237-boss.mysql.database.azure.com',
+    user: 'c237boss',
+    password: 'c237boss!',
+    database: 'c237_005_team1'
+  });
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+// Set up view engine
+app.set('view engine', 'ejs');
+//  enable static files
+app.use(express.static('public'));
+// enable form processing
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    // Session expires after 1 week of inactivity
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 } 
+}));
+
+app.use(flash());
+
+//TO DO add middleware to check if user is logged in
+
+//TO DO add middleware to check if user is admin
+
+
+const validateRegistration = (req, res, next) => {
+    const { username, email, password, address, contact, role } = req.body;
+
+    if (!username || !email || !password || !address || !contact || !role) {
+        return res.status(400).send('All fields are required.');
+    }
+    
+    if (password.length < 6) {
+        req.flash('error', 'Password should be at least 6 or more characters long');
+        req.flash('formData', req.body);
+        return res.redirect('/register');
+    }
+    next();
+};
+
+//TO DO define routes
+
+
 app.get('/deleteHabit/:id', (req, res) => {
     const habitId = req.params.id;
 
@@ -11,3 +86,8 @@ app.get('/deleteHabit/:id', (req, res) => {
         }
     });
 });
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
